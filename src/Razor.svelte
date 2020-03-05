4fc9aa7f6875a34e74ffb07144920e4434f68f16
Bill <bill.kurtson@sj.se>
2020-03-05 21:19:34 +0100

    Because: - the favicon matches the app's usage - firebase needs external scripts to work - using constants makes for better maintainability and readability - colors helps the user understand how much the blade's been used - a dynamic value is more maintainable and reliable than a boolean

--- src/Razor.svelte
+++ src/Razor.svelte
@@ -1,27 +1,28 @@
 <script>
-    let noOfFlipsRemaining = 1;
-    let noOfUsesOnCurrentSideRemaining = 3;
+    const USES_PER_SIDE = 3;
+    const TOTAL_FLIPS = 1;
+    let noOfFlipsRemaining = TOTAL_FLIPS;
+    let remainingUsesOnCurrentSide = USES_PER_SIDE;
     let usable = true;
 
     function flip() {
         noOfFlipsRemaining -= 1;
-        noOfUsesOnCurrentSideRemaining = 3;
+        remainingUsesOnCurrentSide = USES_PER_SIDE;
     }
 
     function use() {
-        noOfUsesOnCurrentSideRemaining -= 1;
+        remainingUsesOnCurrentSide -= 1;
     }
 
-    function disable() {
-        usable = false;
+    function resetBlade() {
+        noOfFlipsRemaining = TOTAL_FLIPS;
+        remainingUsesOnCurrentSide = USES_PER_SIDE;
     }
 
     function updateState() {
-        if (noOfUsesOnCurrentSideRemaining < 1) {
+        if (remainingUsesOnCurrentSide < 1) {
             if (noOfFlipsRemaining > 0)
                 flip();
-            else
-                disable();
         }
     }
 
@@ -29,16 +30,84 @@
         use();
         updateState();
     }
+
+    $: razorbladeStateClass = () => {
+        const classPrefix = 'razorblade--';
+        const classModifier = remainingUsesOnCurrentSide + (noOfFlipsRemaining * USES_PER_SIDE);
+
+        return classPrefix + classModifier;
+    };
+
+    $: usable = () => remainingUsesOnCurrentSide > 0;
+
+    $: isTimeToFlip = () => remainingUsesOnCurrentSide === USES_PER_SIDE && noOfFlipsRemaining < 1;
 </script>
 
 <div>
     <h3>My razor</h3>
-    {#if usable}
+    <h2>
+        {#if noOfFlipsRemaining}
+            Side 1
+        {:else}
+            Side 2
+        {/if}
+    </h2>
+
+    {#if usable()}
         <button on:click={shave}>Shave</button>
     {:else}
-        <div>This razor is trash. Put on a new one!</div>
+        <h3>This razor is trash!</h3>
+        <button on:click={resetBlade}>Insert new blade</button>
     {/if}
 
-    <div>Uses remaining on this side: {noOfUsesOnCurrentSideRemaining}</div>
+    <div class="razorblade {razorbladeStateClass()}">
+        <img class="razorblade__image" src="/images/razorblade-128x128.png" alt="razorblade"/>
+    </div>
+
+    {#if isTimeToFlip()}
+        <h3>Flip!</h3>
+    {/if}
+
+    <div>Uses remaining on this side: {remainingUsesOnCurrentSide}</div>
     <div>Flips remaining: {noOfFlipsRemaining}</div>
 </div>
+
+<style>
+    .razorblade {
+        width: 128px;
+        height: 128px;
+        margin: auto;
+    }
+
+    .razorblade__image {
+        width: 100%;
+    }
+
+    .razorblade--0 {
+        background-color: #ff0600;
+    }
+
+    .razorblade--1 {
+        background-color: #ff5400;
+    }
+
+    .razorblade--2 {
+        background-color: #ffaf00;
+    }
+
+    .razorblade--3 {
+        background-color: #FFF200;
+    }
+
+    .razorblade--4 {
+        background-color: #b9d500;
+    }
+
+    .razorblade--5 {
+        background-color: #69b500;
+    }
+
+    .razorblade--6 {
+        background-color: #1E9600;
+    }
+</style>
